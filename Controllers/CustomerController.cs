f1380febf1ffc7cf58766562596ac2c4b44d3772
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2023-01-12 19:39:35 -0300

    FEAT: criado funcionalidade de cadastro de clientes

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tech_test_payment_api.Context;
+using tech_test_payment_api.Models;
+
+namespace tech_test_payment_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerContext _customerCtx;
+        public CustomerController(CustomerContext customerContext)
+        {
+            _customerCtx = customerContext;
+        }
+
+        [HttpPost]
+        public IActionResult CustomerRegister(Customer customer)
+        {
+            Regex CPF = new Regex(@"\d{3}\.?\d{3}\.?\d{3}\-?\d{2}");
+            if (customer.Name == null)
+            {
+                return BadRequest(new { Error = "Invalid or not received customer name" });
+            }
+            if (customer.Address == null)
+            {
+                return BadRequest(new { Error = "Invalid or not received customer address" });
+            }
+            if (customer.CPF == null || !CPF.IsMatch(customer.CPF))
+            {
+                return BadRequest(new { Error = "Invalid or not received customer CPF" });
+            }
+            customer.Active = true;
+            try
+            {
+                _customerCtx.Customers.Add(customer);
+                _customerCtx.SaveChanges();
+                return Ok();
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest(new { Error = "Invalid customer data" });
+            }
+            catch (Exception error)
+            {
+                return Problem(error.Message);
+            }
+        }
+    }
+}
--- Models/Sale.cs
+++ Models/Sale.cs
@@ -3,8 +3,8 @@ namespace tech_test_payment_api.Models
     public class Sale
     {
         public Guid Id { get; set; }
-        public Seller Seller { get; set; }
-        public Customer Customer { get; set; }
+        public Guid Seller { get; set; }
+        public Guid Customer { get; set; }
         public List<string> Items { get; set; }
         public DateTime Date { get; set; }
         public EnumSaleStatus Status { get; set; }
