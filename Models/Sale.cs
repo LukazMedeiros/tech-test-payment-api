deb941404bd65f18f3138c03201622dd943fe633
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2023-01-12 20:59:18 -0300

    REFACTOR: alterado atributos de ID de vendedor e cliente

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -3,8 +3,8 @@ namespace tech_test_payment_api.Models
     public class Sale
     {
         public Guid Id { get; set; }
-        public Guid Seller { get; set; }
-        public Guid Customer { get; set; }
+        public Guid SellerId { get; set; }
+        public Guid CustomerId { get; set; }
         public List<string> Items { get; set; }
         public DateTime Date { get; set; }
         public EnumSaleStatus Status { get; set; }
