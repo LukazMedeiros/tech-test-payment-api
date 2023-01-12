7e08e87a36f6957e28e19aae38c14a402a0eabba
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2023-01-12 20:59:48 -0300

    FEAT: Criado funcionalidade de cadastro de vendas

--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using tech_test_payment_api.Context;
+using tech_test_payment_api.Models;
 
 namespace tech_test_payment_api.Controllers
 {
@@ -15,6 +17,41 @@ namespace tech_test_payment_api.Controllers
             _saleCtx = saleContext;
             _sellerCtx = sellerContext;
             _customerCtx = customerContext;
+        }
+
+        [HttpPost]
+        public IActionResult SaleRegister(Sale sale)
+        {
+            var seller = _sellerCtx.Sellers.Find(sale.SellerId);
+            var customer = _customerCtx.Customers.Find(sale.CustomerId);
+
+            if (sale.Items == null || sale.Items.Count() < 1)
+            {
+                return BadRequest(new { Erro = "Not received item list or quantity less than 1!" });
+            }
+            if (sale.CustomerId == null || !customer.Active)
+            {
+                return BadRequest(new { Erro = "Invalid and/or inactive customer" });
+            }
+            if (sale.SellerId == null || !seller.Active)
+            {
+                return BadRequest(new { Erro = "Invalid and/or inactive seller" });
+            }
+            sale.Status = EnumSaleStatus.AwaitingPayment;
+            try
+            {
+                _saleCtx.Sales.Add(sale);
+                _saleCtx.SaveChanges();
+                return CreatedAtAction(nameof(GetSaleById), new { id = sale.Id }, sale);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest(new { Error = "Invalid sale data" });
+            }
+            catch (Exception error)
+            {
+                return Problem(error.Message);
+            }
         }
 
         [HttpGet("{id}")]
